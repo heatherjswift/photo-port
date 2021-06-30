efd8e15173760eda907c285b64d44b4d38e8f032
Heather Swift <78330855+heatherjswift@users.noreply.github.com>
2021-06-30 12:50:36 -0400

    conditionally render gallery pages

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function categorySelected(name) {
-  console.log(`${name} clicked`)
-}
+function Nav(props) {
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
-function Nav() {
-  const categories = [
-    {
-      name: 'commercial',
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects"
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    {
-      name: 'landscape',
-      description: 'Field, farmhouses, waterfalls, and the beauty of nature',
-    },
-  ];
   return (
-    <header>
+    <header className='flex-row px-1'>
       <h2>
         <a data-testid='link' href="/">
-          <span role='img' aria-label='camera'>📸</span> Oh Snap!
+          <span role='img' aria-label='camera'>
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
         </a>
       </h2>
       <nav>
@@ -36,12 +33,15 @@ function Nav() {
             <span>Contact</span>
           </li>
             {categories.map((category) => (
-              <li
-              className="mx-1"
-              key={category.name}
+              <li className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+              }`} key={category.name}>
+                <span
+                  onClick={() => {
+                    setCurrentCategory(category)
+                  }}
               >
-                <span onClick={() => categorySelected(category.name)} > 
-                  {category.name}
+                {capitalizeFirstLetter(category.name)}
                 </span>
               </li>
             ))}
@@ -50,7 +50,5 @@ function Nav() {
     </header>
   );
 }
-
-
 
 export default Nav;
